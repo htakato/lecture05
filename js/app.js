8ae3293f20f4cac23e6aede4abf059e2e12b474e
htakato <s13526ht@sfc.keio.ac.jp>
2014-05-14 14:11:41 +0900

    課題です

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 var inputoperandA = document.querySelector("#operand_a");
 var inputoperandB = document.querySelector("#operand_b");
-var inputOperator = document.querySelector("#operator");
 var output = document.querySelector("#output");
 var error = document.querySelector("#error");
 
@@ -20,25 +19,8 @@ var add = function(a, b){
 	return a + b;
 };
 
-var subtract = function(a, b){
-	return a - b;
-};
-
 var multiply = function(a, b){
 	return a * b;
-};
-
-var divide = function(a, b){
-	return a / b;
-};
-
-var modulus = function(a, b){
-	return a % b;
-};
-
-var isOperator = function(operator){
-	return operator == "+" || operator == "-" || operator == "*" ||
-	operator == "/" || operator == "%";
 };
 
 var isNumber = function(a){
@@ -57,52 +39,35 @@ var isNaturalNumber = function(a){
 	return isInteger(a) && a > 0;
 };
 
-var isDivision = function(operator, a, b){
-	return operator == "/" && isNumber(a) && isNonZeroNumber(b);
+var isMultiplication = function(a, b){
+	return isNaturalNumber(a) && isNumber(b);
 };
 
-var isModulus = function(operator, a, b){
-	return operator == "%" && isNaturalNumber(a) && isNaturalNumber(b);
-};
-
-var isMultiplication = function(operator, a, b){
-	return operator == "*" && isNumber(a) && isNumber(b);
-};
-
-var isSubtraction = function(operator, a, b){
-	return operator == "-" && isNumber(a) && isNumber(b);
-};
-
-var isAddition = function(operator, a, b){
-	return operator == "+" && isNumber(a) && isNumber(b);
+var isAddition = function(a, b){
+	return isNumber(a) && isNumber(b);
 }
 
-var isReady = function(operator, a, b){
-	return isDivision(operator, a, b) || isModulus(operator, a, b) ||
-	isMultiplication(operator, a, b) || isSubtraction(operator, a, b) || isAddition(operator, a, b);
+var isReady = function(a, b){
+	return isMultiplication(a, b);
 };
 
 var startCalc = function(){
 	var operandA = inputoperandA.value;
 	var operandB = inputoperandB.value;
-	var operator = inputOperator.value;
 
 	operandA = Number(operandA);
-	operandB = Number(operandB);
 
 	hideError();
-	if(isReady(operator, operandA, operandB)){
+	if(isReady(operandA, operandB)){
 		var result = 0;
-		if(operator == "+"){
-			result = add(operandA, operandB);
-		}else if(operator == "-"){
-			result = subtract(operandA, operandB)
-		}else if(operator == "*"){
-			result = multiply(operandA, operandB);
-		}else if(operator == "/"){
-			result = divide(operandA, operandB);
-		}else if(operator == "%"){
-			result = modulus(operandA, operandB);
+		if(operandB == "5%"){
+			result = multiply(operandA, 1.05);
+		}
+		if(operandB == "8%"){
+			result = multiply(operandA, 1.08);
+		}
+		if(operandB == "10%"){
+			result = multiply(operandA, 1.1);
 		}
 		showResult(result);
 	}else{
